49232b6decadf117d3324c11e32ca56312ce8c2f
Raja s <rajaaltus@gmail.com>
2020-10-28 21:10:29 +0700

    dockerized

--- app/config/server.js
+++ app/config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: "https://092fc490ab51.ngrok.io",
+  port: env.int("PORT", 1338),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "d4990afb1882a4903cefaacb35dd18f4"),
