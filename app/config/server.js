b2737973570b58ce7f1da39cc2ab8e08d732559c
Raja s <rajaaltus@gmail.com>
2021-04-30 21:42:17 +0700

    update with month filed in activities

--- app/config/server.js
+++ app/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1338),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "d4990afb1882a4903cefaacb35dd18f4"),
